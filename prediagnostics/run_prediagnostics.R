dc56e0c0424e60b11dc09c6ff435fc38e12b9bd7
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-02-02 16:51:00 -0500

    Update commandline arguments

--- prediagnostics/run_prediagnostics.R
+++ prediagnostics/run_prediagnostics.R
@@ -3,25 +3,27 @@ source("/app/prediagnostics/input.R")
 source("/app/prediagnostics/prediagnostics.R")
 source("/app/prediagnostics/output.R")
 
-start <- Sys.time()
+start = Sys.time()
 
 # Directories
-input_dir <- file.path("/mnt", "data", "input")
-output_dir <- file.path("/mnt", "data", "output")
+input_dir = file.path("/mnt", "data", "input")
+output_dir = file.path("/mnt", "data", "output")
 
 # Command line arguments
 args = commandArgs(trailingOnly=TRUE)
-# reaches_json = ifelse(identical(args, character(0)), "reaches.json", args[1])
-
-if (length(args)>=1){
-    reaches_json = file.path(input_dir, paste('reaches_',strtoi(args[1]),'.json'))
+if (length(args)>=2){
+    index = strtoi(args[1])
+    reaches_json = file.path(input_dir, paste(args[2]))
+} else if (length(args)>=1) {
+    index = strtoi(args[1])
+    reaches_json = file.path(input_dir, 'reaches.json')
 } else{
+    index = strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1
     reaches_json = file.path(input_dir, 'reaches.json')
 }
 
 # Run Diagnostics
-index <- strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1
 run_diagnostics(input_dir, reaches_json, index, output_dir)
 
-end <- Sys.time()
+end = Sys.time()
 print(paste0("Execution time: ", end - start))
