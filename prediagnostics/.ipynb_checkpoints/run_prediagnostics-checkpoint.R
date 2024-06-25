dbbb0de218136aa429822543239c29363c704da1
cjgleason_umass_edu <cjgleason_umass_edu@ceewater-cpu002.unity.rc.umass.edu>
2024-06-25 22:58:56 +0000

    prediag update post ST

---
+++ prediagnostics/.ipynb_checkpoints/run_prediagnostics-checkpoint.R
@@ -0,0 +1,38 @@
+source("/app/prediagnostics/config.R")
+source("/app/prediagnostics/input.R")
+source("/app/prediagnostics/prediagnostics.R")
+source("/app/prediagnostics/output.R")
+
+start = Sys.time()
+
+# Directories
+input_dir = file.path("/mnt", "data", "input")
+output_dir = file.path("/mnt", "data", "output")
+
+# Command line arguments
+args = commandArgs(trailingOnly=TRUE)
+# we want to specify index and reach json for local run
+if (length(args)>=2){
+    index = strtoi(args[1]) + 1
+    reaches_json = file.path(input_dir, paste(args[2]))
+
+    # we want to specify reach json for aws run
+    if (length(args)>=3){
+        index = strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1
+    }
+
+# we want to specify only index for local run
+} else if (length(args)>=1) {
+    index = strtoi(args[1]) + 1
+    reaches_json = file.path(input_dir, 'reaches.json')
+    # we want to run on default settings for aws
+} else{
+    index = strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1
+    reaches_json = file.path(input_dir, 'reaches.json')
+}
+
+# Run Diagnostics
+run_diagnostics(input_dir, reaches_json, index, output_dir)
+
+end = Sys.time()
+print(paste0("Execution time: ", end - start))
