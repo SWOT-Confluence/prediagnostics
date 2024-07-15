aed912c2ee7e965ec2d39b1a8fc205dccbc5e241
Travis-Simmons <travisthomassimmons@gmail.com>
2024-07-15 14:36:52 +0000

    baseline config

--- prediagnostics/config.R
+++ prediagnostics/config.R
@@ -1,15 +1,15 @@
 GLOBAL_PARAMS = list (
-  Tukey_number = 1.5,
-  ice_max = 0,
-  dark_max = 0.1,
-  xover_cal_q_max = 1,
-  slope_min = 1.7e-5,
-  prior_width_min = 80 ,
-  target_bit= 168298510,
-  cross_track_dist_min_m= 15000,
-  reach_length_min_m=7000,
-  wse_r_u_max = 0.5,
-  slope_r_u_max=10e-5  
+ wse_r_u_max = 0.5,
+ dark_max = 0.1,
+ slope_min = 1.7e-5,
+ Tukey_number = 1.5,
+ ice_max = 0,
+ xover_cal_q_max = 1,
+ prior_width_min = 80 ,
+ target_bit= 168298510,
+ cross_track_dist_min_m= 15000,
+ reach_length_min_m=7000,
+ slope_r_u_max=10e-5  
 )
 
 # wse_r_u < 0.5 m and slope_r_u < 10 cm/km
