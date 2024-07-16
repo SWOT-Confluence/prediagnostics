3242e33b4852362ab66ca68dc106a9fc2a6e5752
Travis-Simmons <travisthomassimmons@gmail.com>
2024-07-16 15:09:54 +0000

    perturb 1

--- prediagnostics/config.R
+++ prediagnostics/config.R
@@ -1,7 +1,8 @@
+# Perturb 1
 GLOBAL_PARAMS = list (
- wse_r_u_max = 0.5,
- dark_max = 0.1,
- slope_min = 1.7e-5,
+ wse_r_u_max = 0.8,
+ dark_max = 0.2,
+ slope_min = 0,
  Tukey_number = 1.5,
  ice_max = 0,
  xover_cal_q_max = 1,
@@ -12,15 +13,48 @@ GLOBAL_PARAMS = list (
  slope_r_u_max=10e-5  
 )
 
-# wse_r_u < 0.5 m and slope_r_u < 10 cm/km
+# Baseline
+# GLOBAL_PARAMS = list (
+#  wse_r_u_max = 0.5,
+#  dark_max = 0.1,
+#  slope_min = 1.7e-5,
+#  Tukey_number = 1.5,
+#  ice_max = 0,
+#  xover_cal_q_max = 1,
+#  prior_width_min = 80 ,
+#  target_bit= 168298510,
+#  cross_track_dist_min_m= 15000,
+#  reach_length_min_m=7000,
+#  slope_r_u_max=10e-5  
+# )
 
- # Tukey_number = 1.5, outlier distance
- #  ice_flag = 0,, ice flag value, set to 0 (no ice)
- #  dark_max = 0.1, maximum dark water
- #  xovr_cal_q_max = 1, quality of hte crossover calibration (1 or less is good)
- #  slope_min = 1.7e-5, minimum slope
- #  prior_width_min = 80 , minimum prior width allowable
- #  bitwise_max= 15, maximum bitwise flag
- #  cross_track_dist_max_km= 15, maximum distace in the cross track
- #  reach_length_min_km=7 minimum reach length
+# Perturb 1
+# GLOBAL_PARAMS = list (
+#  wse_r_u_max = 0.8,
+#  dark_max = 0.2,
+#  slope_min = 0,
+#  Tukey_number = 1.5,
+#  ice_max = 0,
+#  xover_cal_q_max = 1,
+#  prior_width_min = 80 ,
+#  target_bit= 168298510,
+#  cross_track_dist_min_m= 15000,
+#  reach_length_min_m=7000,
+#  slope_r_u_max=10e-5  
+# )
 
+
+# Perturb 2
+# GLOBAL_PARAMS = list (
+#  wse_r_u_max = 0.8,
+#  dark_max = 0.1,
+#  slope_min = 0,
+#  Tukey_number = 1.5,
+#  ice_max = 0,
+#  xover_cal_q_max = 1,
+#  prior_width_min = 80 ,
+#  target_bit= 168298510,
+#  cross_track_dist_min_m= 15000,
+#  reach_length_min_m=7000,
+#  slope_r_u_max=10e-5  
+# )
