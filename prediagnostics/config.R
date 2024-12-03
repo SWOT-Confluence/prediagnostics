3bad73c8bf73f44bb72a761813be993008630a48
cjgleason <cjgleason@umass.edu>
2024-12-02 22:10:41 -0500

    Update config.R

--- prediagnostics/config.R
+++ prediagnostics/config.R
@@ -1,16 +1,31 @@
 
 
 # Baseline
+#GLOBAL_PARAMS = list (
+# wse_r_u_max = 0.5,
+# dark_max = 0.1,
+# slope_min = 1.7e-5,
+# Tukey_number = 1.5,
+# ice_max = 0,
+# xover_cal_q_max = 1,
+# prior_width_min = 80 ,
+# target_bit= 168298510,
+# cross_track_dist_min_m= 15000,
+# reach_length_min_m=7000,
+# slope_r_u_max=10e-5  
+#)
+
+# Dec 2 2024 v 0002 params
 GLOBAL_PARAMS = list (
  wse_r_u_max = 0.5,
- dark_max = 0.1,
+ dark_max = 0.4,
  slope_min = 1.7e-5,
  Tukey_number = 1.5,
  ice_max = 0,
  xover_cal_q_max = 1,
  prior_width_min = 80 ,
  target_bit= 168298510,
- cross_track_dist_min_m= 15000,
+ cross_track_dist_min_m= 10000,
  reach_length_min_m=7000,
  slope_r_u_max=10e-5  
 )
